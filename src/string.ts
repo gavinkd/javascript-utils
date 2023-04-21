bba00254721b95ec434ee04923405cd54f47c870
Frank <zzhao8053@gmail.com>
2023-04-21 09:21:39 +0800

    init

--- src/string.ts
+++ src/string.ts
@@ -5,21 +5,32 @@
  * @param start 起始位置，默认下标为3
  * @param slide 向后的位数，默认后4位
  * @param padding 填充物，默认为*
+ * @returns {string}
  */
-export function telephoneFormat(value: string, start: number = 3, slide: number = 4, padding: string = "*"): string {
-  const startReg = "(\\d{" + start + "})"
-  const paddingContent =  new Array(slide > value.length ? value.length - start : slide).fill(padding).join("")
-  let reg = "";
-  let replaceValue = "";
-  if(slide < value.length) {
+export function telephoneFormat(value: string, start: number = 3, slide: number = 4, padding: string = '*'): string {
+  const startReg = '(\\d{' + start + '})'
+  const paddingContent = new Array(slide > value.length ? value.length - start : slide).fill(padding).join('')
+  let reg = ''
+  let replaceValue = ''
+  if (slide < value.length) {
     const end = slide > value.length ? value.length : value.length - start - slide
-    reg = startReg + "\\d{" + slide + "}" +  "(\\d{" + end + "})"
+    reg = startReg + '\\d{' + slide + '}' + '(\\d{' + end + '})'
     replaceValue = `$1${paddingContent}$2`
   }
-  if(slide >= value.length) {
-    reg = startReg + "\\d{" + (value.length - start) + "}"
+  if (slide >= value.length) {
+    reg = startReg + '\\d{' + (value.length - start) + '}'
     replaceValue = `$1${paddingContent}`
   }
-  const regExp = new RegExp(reg)
-  return value.replace(regExp, replaceValue)
+  return value.replace(new RegExp(reg), replaceValue)
+}
+
+/**
+ * [_/./-]转驼峰命名
+ * @param str
+ * @returns {string}
+ */
+export function camelize(str: string) {
+  return str.replace(/[_.-](\w|$)/g, function (_, x) {
+    return x.toUpperCase()
+  })
 }
